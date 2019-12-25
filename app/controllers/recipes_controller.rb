3e5adcc093399f0564d3914aab52c329b55ff433
strahinja <djuricastrahinja@gmail.com>
2019-12-25 02:52:16 +0100

    Add admin feature, handle restrictions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -52,8 +52,8 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
   def require_same_user
-    if current_chef != @recipe.chef
-      flash[:danger] = "You can only edit or delete your own recipes!"
+    if current_chef != @recipe.chef and !current_chef.admin?
+      flash[:danger] = "You can only edit or delete your own recipes"
       redirect_to recipes_path
     end
   end
--- test/integration/recipes_edit_test.rb
+++ test/integration/recipes_edit_test.rb
@@ -2,32 +2,43 @@ require 'test_helper'
 
 class RecipesEditTest < ActionDispatch::IntegrationTest
   def setup
-    @chef = Chef.create!(chefname: "mashrur", email: "mashrur@example.com",
-                         password: "password", password_confirmation: "password")
-    @recipe = Recipe.create(name: "vegetable saute", description: "great vegetable sautee, add vegetable and oil", chef: @chef)
+    @chef = Chef.create!(chefname: "mashrur",
+                            email: "mashrur@example.com",
+                          password: "password",
+                              password_confirmation: "password")
+    @chef2 = Chef.create!(chefname: "john",
+                                  email: "john@example.com",
+                        password: "password",
+                              password_confirmation: "password")
+    @admin_user = Chef.create!(chefname: "john1",
+                                email: "john1@example.com",
+                        password: "password",
+                      password_confirmation: "password", admin: true)
   end
 
-  test "reject invalid recipe update" do
-    sign_in_as(@chef, "password")
-    get edit_recipe_path(@recipe)
+  test "accept edit attempt by admin user" do
+    sign_in_as(@admin_user, "password")
+    get edit_chef_path(@chef)
     assert_response :success
-    patch recipe_path(@recipe), params: { recipe: { name: " ", description: "some description" } }
-    assert_response :success
-    assert_select 'h2.panel-title'
-    assert_select 'div.panel-body'
-  end
-  test "successfully edit a recipe" do
-    sign_in_as(@chef, "password")
-    get edit_recipe_path(@recipe)
-    assert_response :success
-    updated_name = "updated recipe name"
-    updated_description = "updated recipe description"
-    patch recipe_path(@recipe), params: { recipe: { name: updated_name, description: updated_description } }
-    assert_redirected_to @recipe
-    follow_redirect!
+    patch chef_path(@chef), params: { chef: { chefname: "mashrur3",
+                                  email: "mashrur3@example.com" } }
+    assert_redirected_to @chef
     assert_not flash.empty?
-    @recipe.reload
-    assert_match updated_name, @recipe.name
-    assert_match updated_description, @recipe.description
+    @chef.reload
+    assert_match "mashrur3", @chef.chefname
+    assert_match "mashrur3@example.com", @chef.email
+  end
+
+  test "redirect edit attempt by another non-admin user" do
+    sign_in_as(@chef2, "password")
+    updated_name = "joe"
+    updated_email = "joe@example.com"
+    patch chef_path(@chef), params: { chef: { chefname: updated_name,
+                                  email: updated_email } }
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "mashrur", @chef.chefname
+    assert_match "mashrur@example.com", @chef.email
   end
 end
